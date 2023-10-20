b190ba0691a1b8635e2af8481b39bd4b6aa6bd0d
Jhansi <jhansisridevi@gmail.com>
2023-10-20 13:58:38 -0400

    Updated file with learner data

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ const AssignmentGroup = {
       points_possible: 500,
     },
   ],
-  //Adding test case to check exception on course-id
+  //Adding test case to check exception on course_id
   //   id: 12346,
   //   name: "JavaScript Callbacks",
   //   course_id: 452,
@@ -93,7 +93,6 @@ const LearnerSubmissions = [
   //     },
   //   },
 ];
-
 function validateLearnerSubmission(submission, subDate, assignment, dueDate) {
   if (
     typeof submission.score !== "number" ||
@@ -101,12 +100,17 @@ function validateLearnerSubmission(submission, subDate, assignment, dueDate) {
   ) {
     throw new Error("Invalid submission data.");
   }
-  console.log(subDate);
-  console.log(dueDate);
+  //console.log(subDate );
+  //console.log(dueDate);
   if (subDate > dueDate) {
     //console.log("Inside if");
     //options for validating 10%
     submission.score -= 0.1 * assignment.points_possible;
+  }
+}
+function validateAssignmentGroup(assignmentGroup, courseInfo) {
+  if (assignmentGroup.course_id !== courseInfo.id) {
+    throw new Error("Assignment Group doesn't belong to the specified course.");
   }
 }
 function validateCourse(assignmentGroup, courseInfo) {
@@ -115,55 +119,93 @@ function validateCourse(assignmentGroup, courseInfo) {
       `Assignment Group "${assignmentGroup.name}" ID: ${assignmentGroup.id} Course ID: ${assignmentGroup.course_id} does not belong to course "${courseInfo.name}" ID: ${courseInfo.id}`
     );
   }
-  const result = getLearnerData(
-    CourseInfo,
-    AssignmentGroup,
-    LearnerSubmissions
-  );
   return true;
 }
 try {
   validateCourse(AssignmentGroup, CourseInfo);
-  console.log("Assignment Group is valid.");
+  //console.log("Assignment Group is valid.");
 } catch (error) {
   console.error(error.message);
 }
-//function calculateScores(course, assignmentGroup, submissions){}
-//console.log(result);
-
-function getLearnerData(
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
+  const result = [];
+  const assignmentScores = {};
+  let totalPoints = 0;
+  const assignmentData = {};
+  assignmentGroup.assignments.forEach((assignment) => {
+    assignmentData[assignment.id] = {
+      name: assignment.name,
+      points_possible: assignment.points_possible,
+      due_at: new Date(assignment.due_at),
+    };
+  });
+  //console.log(assignmentData);
+  const learnerIds = [
+    ...new Set(learnerSubmissions.map((submission) => submission.learner_id)),
+  ]; //to store unique values, considering id to be primitive
+  //console.log(learnerIds);
+  for (const learnerId of learnerIds) {
+    const learnerData = {
+      id: learnerId,
+      avg: 0,
+    };
+    for (const assignment of assignmentGroup.assignments) {
+      const submission = learnerSubmissions.find(
+        (sub) =>
+          sub.assignment_id === assignment.id && sub.learner_id === learnerId
+      );
+      if (!submission) {
+        continue;
+      }
+      const dueDate = new Date(assignment.due_at);
+      const subDate = new Date(submission.submission.submitted_at);
+      if (new Date(submission.submitted_at) < dueDate) {
+        //submissionDate validation
+        continue;
+      }
+      if (dueDate > new Date()) {
+        break;
+        //console.log("Due not now")
+      }
+      validateLearnerSubmission(
+        submission.submission,
+        subDate,
+        assignment,
+        dueDate
+      );
+      assignmentScores[assignment.id] =
+        submission.submission.score / assignment.points_possible;
+      //console.log("Here: "+assignmentScores[assignment.id]);
+      totalPoints += assignmentData[assignment.id].points_possible;
+      //console.log("Total points: "+totalPoints);
+      const totalWeightedScore = Object.values(assignmentScores).reduce(
+        (total, score) => total + score,
+        0
+      );
+      assignmentGroup.assignments.forEach((assignment) => {
+        const assignmentId = assignment.id;
+        const assignmentScore = assignmentScores[assignment.id];
+        if (assignmentScore) {
+          learnerData[assignmentId] = assignmentScore;
+          //(assignmentScore / assignmentData[assignmentId].points_possible) * 100;
+          //console.log("Learner info: "+assignmentId+"-"+learnerData[assignmentId]);
+        }
+        if (totalPoints === 0) {
+          //validating to be changed when the try catch handles assignment-courses
+          learnerData.avg = 0;
+        } else {
+          learnerData.avg = (totalWeightedScore / totalPoints) * 100;
+          //console.log(Math.round(learnerData.avg)); - needs change
+        }
+      });
+    }
+    result.push(learnerData);
+  }
+  return result;
+}
+const learnerData = getLearnerData(
   CourseInfo,
   AssignmentGroup,
-  LearnerSubmissions,
-  learnerId
-) {
-  try {
-    const assignmentScores = calculateScores(
-      learnerId,
-      AssignmentGroup,
-      LearnerSubmissions
-    );
-    console.log(assignmentScores);
-  } catch (error) {
-    throw new Error(`Error: ${error.message}`);
-  }
-}
-// const learnerId = 132; provided learner id
-//
-try {
-  const result = getLearnerData(
-    CourseInfo,
-    AssignmentGroup,
-    LearnerSubmissions,
-    learnerId
-  );
-  console.log(result);
-} catch (error) {}
-
-/*
-return {
-    id: learnerId,
-    weightedAvg,
-    ...assignmentScores,
-  };
-  */
+  LearnerSubmissions
+);
+console.log(learnerData);
